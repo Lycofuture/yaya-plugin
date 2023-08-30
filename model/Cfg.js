225453a6ca17b1d44982ebe391ce08aab1942589
Lycofuture <hizcmua@163.com>
2023-08-30 16:20:26 +0800

    Merge remote-tracking branch 'origin/main'

--- model/Cfg.js
+++ model/Cfg.js
@@ -1,18 +1,17 @@
 import fs from 'node:fs'
 
-// import lodash from 'lodash'
 class Cfg {
-    constructor() {
-        /** 默认设置 */
-        this.defSetPath = `./plugins/${this.package.name}/defSet/`
+  constructor () {
+    this.pluginPath = `${this.package._path}/plugins/${this.package.name}/`
+    /** 默认设置 */
+    this.defSetPath = `${this.pluginPath}defSet/`
 
-        /** 用户设置 */
-        this.configPath = `./plugins/${this.package.name}/config/`
-        /** 初始化 **/
-        console.log(this.package)
-    }
+    /** 用户设置 */
+    this.configPath = `${this.pluginPath}config/`
+    /** 初始化 **/
+  }
 
-    /**
+  /**
      * 获取包信息
      * @property {string} _path - BOT根目录
      * @property {string} name - 插件名字
@@ -23,13 +22,46 @@ class Cfg {
      * version: string
      * }}
      */
-    get package() {
-        const _path = process.cwd()
-        const data = JSON.parse(fs.readFileSync(`../package.json`, 'utf8'))
-        const name = data.name
-        const version = data.version
-        return {_path, name, version}
+  get package () {
+    const _path = process.cwd()
+    const data = JSON.parse(fs.readFileSync('plugins/yaya-plugin/package.json', 'utf8'))
+    const name = data.name
+    const version = data.version
+    return {
+      _path,
+      name,
+      version
     }
+  }
+
+  /**
+     * 模板配置文件
+     * @property {string} _path - 资源
+     * @property {string} helpCfg - 标题
+     * @property {string} helpList - 指令列表
+     * @property {string} css - css
+     * @property {string} html 模板路径
+     */
+  async deploy () {
+    const yaya_path = `${this.pluginPath}resources/`
+    const defaultLayout = `${yaya_path}common/layout/default.html`
+    const html = `${yaya_path}html/help/help.html`
+    const data = await import(`file://${this.defSetPath}help/help.js`)
+    const css = `${yaya_path}html/help/help.css`
+    const colCount = Math.min(5, Math.max(3, 2))
+    const plugin = this.package.name
+    const version = this.package.version
+    return {
+      defaultLayout,
+      yaya_path,
+      css,
+      ...data,
+      colCount,
+      html,
+      plugin,
+      version
+    }
+  }
 }
 
 export default new Cfg()
