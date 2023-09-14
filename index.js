7ab0a401594cb903b8fb5ce285a635e8ca78d8fe
Lycofuture <hizcmua@163.com>
2023-09-14 22:31:21 +0800

    feat: 增加米游社签到

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 import fs from 'fs'
-import { segment } from 'icqq'
+import {segment} from 'icqq'
+import Cfg from "./model/Cfg.js";
+
 global.segment = segment
-const files = fs.readdirSync('./plugins/yaya-plugin/apps').filter(file => file.endsWith('.js'))
+const files = fs.readdirSync(`./plugins/${Cfg.package.name}/apps`).filter(file => file.endsWith('.js'))
 
 let ret = []
 
 files.forEach((file) => {
-  ret.push(import(`./apps/${file}`))
+    ret.push(import(`./apps/${file}`))
 })
 
 ret = await Promise.allSettled(ret)
 
 const apps = {}
 for (const i in files) {
-  const name = files[i].replace('.js', '')
+    const name = files[i].replace('.js', '')
 
-  if (ret[i].status !== 'fulfilled') {
-    logger.error(`载入插件错误：${logger.red(name)}`)
-    logger.error(ret[i].reason)
-    continue
-  }
-  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+    if (ret[i].status !== 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
-export { apps }
+logger.info(Cfg.package.name, '初始化完成~~~')
+export {apps}
