4ca425e2ff2a86ea7cf80ca5eb46a09144ce3c7f
Lycofuture <hizcmua@163.com>
2023-09-19 21:13:53 +0800

    更新css

---
+++ apps/index.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+import { app, Data, Plugin } from '../model/index.js'
+
+const files = fs.readdirSync(`${Data._path('yaya')}apps`).filter(file => file.endsWith('.js') && file !== 'index.js')
+
+let ret = []
+
+files.forEach((file) => {
+  ret.push(import(`./${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
+const apps = {}
+let num = 0
+for (const i in files) {
+  const name = files[i].replace('.js', '')
+
+  if (ret[i].status !== 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+  await redis.set(app + i, JSON.stringify(new apps[name]()))
+  num++
+}
+await redis.set(`${app}num`, num)
+
+logger.info(Plugin.name, '初始化完成~~~')
+export { apps, ret }
